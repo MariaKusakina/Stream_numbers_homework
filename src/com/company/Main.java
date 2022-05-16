5c7a4f8101fc377d517bbea9477ef6ecdcdc9dd9
MariaKusakina <m.a.kusakina@gmail.com>
2022-05-16 21:53:02 +0300

    задача решена

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        List<Integer> positiveNum = new ArrayList<>();
+        intList.forEach(number -> {
+            if(number > 0 && number % 2 == 0) {
+                positiveNum.add(number);
+            }
+        });
+        positiveNum.sort(Comparator.naturalOrder());
+        positiveNum.forEach(numbers -> {
+            System.out.println(numbers);
+        });
     }
 }
